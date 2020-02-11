29da87f395e129683281c8433ce5a76a5f9715b0
Gene Campbell III <gcampbell391@gmail.com>
2020-02-11 23:57:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ require "yaml"
 def load_library(file_path)
   emoticons_file = YAML.load_file(file_path)
   new_hash = {"get_meaning": {},"get_emoticon": {}}
-  emoticons_file.each
+  emoticons_file.each do ||
   
   
 end
