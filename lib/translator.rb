06dd22f803f417fd18714eebecd0819879a8d8a8
Gene Campbell III <gcampbell391@gmail.com>
2020-02-11 23:59:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,8 @@ def load_library(file_path)
   new_hash = {"get_meaning": {},"get_emoticon": {}}
   emoticons_file.each do |meaning, emoticons|
     new_hash["get_meaning"][emoticons[1]] = meaning
-    new_hash["get_emoticon"][emoticons[0]] = emoticon
+    new_hash["get_emoticon"][emoticons[0]] = emoticons[1]
+    
   
   
 end
