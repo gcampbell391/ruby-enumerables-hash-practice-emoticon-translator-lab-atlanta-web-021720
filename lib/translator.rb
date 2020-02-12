bfda98f9093364ac080d7cdade1216e8a0d5c356
Gene Campbell III <gcampbell391@gmail.com>
2020-02-12 00:22:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,9 @@ def load_library(emoticon_file)
 end
 
 def get_japanese_emoticon(emoticon_file, emoticon)
-  # code goes here
+  emoticon_lib = load_library(emoticon_file)
+  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
+  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
 end
 
 def get_english_meaning(emoticon, emoticon)
