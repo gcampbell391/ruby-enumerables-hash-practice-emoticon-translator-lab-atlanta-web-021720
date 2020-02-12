364250cbf69524e179e8442fc1f815cc466d2582
Gene Campbell III <gcampbell391@gmail.com>
2020-02-12 00:17:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,17 @@ emoticon_file = YAML.load_file('./lib/emoticons.yml')
 
 
 def load_library(emoticon_file)
-  emoticons = emoticon_file
-  
+    emoticons = emoticon_file
+    emoticon_final = {'get_meaning'  => {},
+                  'get_emoticon' => {} }
+
+  emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
+    english = value[0]
+    japanese = value[1]
+    emoticon_final['get_meaning'][japanese] = meaning
+    emoticon_final['get_emoticon'][english] = japanese
+  end
+  emoticon_lib
 end
 
 def get_japanese_emoticon(path_file, emoticon)
